75292035d52e55b64699273c927cedbc8f14ae80
HoridasPro <horidassarker6@gmail.com>
2026-05-28 10:32:53 +0600

    added to problem solving 7

---
+++ problem-solving-7.ts
@@ -0,0 +1,16 @@
+// Given an array of strings, group all anagrams together.
+
+// Anagrams are words made from the same letters.
+
+const anagramString = (strs: string[]) => {
+  const map = new Map();
+  for (let str of strs) {
+    const key = str.split("").sort().join("");
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(str);
+  }
+  return Array.from(map.values());
+};
+console.log(anagramString(["eat", "tea", "tan", "ate", "nat", "bat"]));
